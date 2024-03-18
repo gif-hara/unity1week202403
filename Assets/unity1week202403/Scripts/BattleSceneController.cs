c242c4d161e7f773c059dc82a40ff873fa275872
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-18 23:21:08 +0900

    バトルシーンの制御を変更

--- Assets/unity1week202403/Scripts/BattleSceneController.cs
+++ Assets/unity1week202403/Scripts/BattleSceneController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace unity1week202403
@@ -13,10 +15,51 @@ namespace unity1week202403
         [SerializeField]
         private Actor actor;
 
-        private void Start()
+        private async void Start()
         {
             var player = actor.Spawn(debugData.PlayerStatus);
             var enemy = actor.Spawn(debugData.EnemyStatus);
+
+            var actorQueue = new Queue<Actor>();
+
+            if (player.StatusController.Speed == enemy.StatusController.Speed)
+            {
+                var random = UnityEngine.Random.Range(0, 2);
+                actorQueue.Enqueue(random == 0 ? player : enemy);
+                actorQueue.Enqueue(random == 0 ? enemy : player);
+            }
+            else
+            {
+                actorQueue.Enqueue(player.StatusController.Speed > enemy.StatusController.Speed ? player : enemy);
+                actorQueue.Enqueue(player.StatusController.Speed < enemy.StatusController.Speed ? player : enemy);
+            }
+
+            while (!player.StatusController.IsDead && !enemy.StatusController.IsDead)
+            {
+                var currentActor = actorQueue.Dequeue();
+                var target = currentActor == player ? enemy : player;
+
+                try
+                {
+                    await currentActor.PerformActionAsync(target, destroyCancellationToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                    break;
+                }
+
+                if (target.StatusController.IsDead)
+                {
+                    break;
+                }
+
+                actorQueue.Enqueue(currentActor);
+            }
         }
     }
 }
