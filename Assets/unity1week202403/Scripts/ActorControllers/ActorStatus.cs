10073d7974acd36837f1c8d312ec560d053ead6d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-18 22:31:49 +0900

    Add actor

---
+++ Assets/unity1week202403/Scripts/ActorControllers/ActorStatus.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+namespace unity1week202403
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ActorStatus
+    {
+        [SerializeField]
+        private int hitPoint;
+
+        [SerializeField]
+        private int physicalAttack;
+
+        [SerializeField]
+        private int physicalDefense;
+
+        [SerializeField]
+        private int magicalAttack;
+
+        [SerializeField]
+        private int magicalDefense;
+
+        [SerializeField]
+        private int speed;
+
+        public int HitPoint => hitPoint;
+
+        public int PhysicalAttack => physicalAttack;
+
+        public int PhysicalDefense => physicalDefense;
+
+        public int MagicalAttack => magicalAttack;
+
+        public int MagicalDefense => magicalDefense;
+
+        public int Speed => speed;
+    }
+}
