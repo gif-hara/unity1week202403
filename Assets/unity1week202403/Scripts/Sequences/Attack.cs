057fb644a82fb37b74302cbf791168273865d896
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-19 00:03:32 +0900

    Attack.csの変更: PlayAsyncメソッドを追加

--- Assets/unity1week202403/Scripts/Sequences/Attack.cs
+++ Assets/unity1week202403/Scripts/Sequences/Attack.cs
@@ -1,9 +1,20 @@
-namespace MH2
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnitySequencerSystem;
+
+namespace unity1week202403
 {
     /// <summary>
     /// 
     /// </summary>
-    public sealed class Attack
+    public sealed class Attack : ISequence
     {
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var owner = container.Resolve<Actor>("OwnerActor");
+            var target = container.Resolve<Actor>("TargetActor");
+
+            return UniTask.CompletedTask;
+        }
     }
 }
