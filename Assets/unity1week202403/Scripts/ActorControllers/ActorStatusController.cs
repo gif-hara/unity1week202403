db9f2b8bd0529c8f10314d43284906c6ba031822
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-20 22:12:52 +0900

    ActorStatusControllerの変更: スキルシーケンスの取得と行動回数の追加

--- Assets/unity1week202403/Scripts/ActorControllers/ActorStatusController.cs
+++ Assets/unity1week202403/Scripts/ActorControllers/ActorStatusController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnitySequencerSystem;
+
 namespace unity1week202403
 {
     /// <summary>
@@ -26,6 +30,23 @@ namespace unity1week202403
 
         public int Speed => status.speed;
 
+        /// <summary>
+        /// 行動した回数
+        /// </summary>
+        public int PerformedActionCount { get; private set; }
+
         public bool IsDead => HitPoint <= 0;
+
+        public void IncrementPerformedActionCount()
+        {
+            PerformedActionCount++;
+        }
+
+        public async UniTask<List<ISequence>> GetSkillSequence()
+        {
+            var index = status.skillIds.Count - 1 < PerformedActionCount ? status.skillIds.Count - 1 : PerformedActionCount;
+            var skillSpec = TinyServiceLocator.Resolve<MasterData>().SkillSpecs.Get(status.skillIds[index]);
+            return (await skillSpec.LoadSkillSequences()).Sequences;
+        }
     }
 }
