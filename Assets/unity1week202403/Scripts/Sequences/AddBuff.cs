1ae8eeb74d4e6fdc46b8a22bc4328abcf2438c9a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-20 22:52:14 +0900

    AddBuffクラスを追加しました

---
+++ Assets/unity1week202403/Scripts/Sequences/AddBuff.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace unity1week202403
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class AddBuff : ISequence
+    {
+        [SerializeField]
+        private Define.TargetType targetType;
+
+        [SerializeField]
+        private Define.BuffType buffType;
+
+        [SerializeField]
+        private int addValue;
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            switch (targetType)
+            {
+                case Define.TargetType.Self:
+                    var owner = container.Resolve<Actor>("OwnerActor");
+                    owner.StatusController.AddBuff(buffType, addValue);
+                    break;
+                case Define.TargetType.Opponent:
+                    var target = container.Resolve<Actor>("TargetActor");
+                    target.StatusController.AddBuff(buffType, addValue);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
