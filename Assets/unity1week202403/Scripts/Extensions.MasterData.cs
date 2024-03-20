cddb0b6ed2a8ce56ff9dc835bfa96cc5c1a4affe
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-20 18:36:32 +0900

    unity1week202403/Scripts/Extensions.MasterData.csとunity1week202403/Scripts/Extensions.MasterData.cs.metaを追加

---
+++ Assets/unity1week202403/Scripts/Extensions.MasterData.cs
@@ -0,0 +1,16 @@
+using Cysharp.Threading.Tasks;
+using UnitySequencerSystem;
+
+namespace unity1week202403
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static UniTask<ScriptableSequences> LoadAsync(this MasterData.SkillSpec self)
+        {
+            return AssetLoader.LoadAsync<ScriptableSequences>($"Assets/unity1week202403/Database/Skill.{self.Id}.asset");
+        }
+    }
+}
