400a4a971972fdbe8c8b130f29db1fa98f690ee0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-20 18:21:40 +0900

    unity1week202403/Scripts/MasterData.csを追加

---
+++ Assets/unity1week202403/Scripts/MasterData.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace unity1week202403
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(fileName = "MasterData", menuName = "unity1week202403/MasterData")]
+    public sealed class MasterData : ScriptableObject
+    {
+        [SerializeField]
+        private CharacterSpec.DictionaryList characterSpecs;
+
+        public CharacterSpec.DictionaryList CharacterSpecs => characterSpecs;
+
+#if UNITY_EDITOR
+        [ContextMenu("Update")]
+        public async void Test()
+        {
+            Debug.Log("Begin MasterData Update");
+            var database = await UniTask.WhenAll(
+                GoogleSpreadSheetDownloader.DownloadAsync("CharacterSpec")
+            );
+
+            characterSpecs.Set(JsonHelper.FromJson<CharacterSpec>(database[0]));
+
+            UnityEditor.EditorUtility.SetDirty(this);
+            UnityEditor.AssetDatabase.SaveAssets();
+            Debug.Log("End MasterData Update");
+        }
+#endif
+
+        [Serializable]
+        public class CharacterSpec
+        {
+            public string Character;
+
+            public int HitPoint;
+
+            public int PhysicalAttack;
+
+            public int PhysicalDefense;
+
+            public int MagicalAttack;
+
+            public int MagicalDefense;
+
+            public int Speed;
+
+            public int Skill1;
+
+            public int Skill2;
+
+            public int Skill3;
+
+            [Serializable]
+            public sealed class DictionaryList : DictionaryList<string, CharacterSpec>
+            {
+                public DictionaryList() : base(x => x.Character) { }
+            }
+        }
+    }
+}
