1081d1aa89fe0dbc7f6eb38933ec0ac8cd1ed475
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-23 08:46:47 +0900

    SelectWordシーンにselectWordDocumentPrefabを追加

--- Assets/unity1week202403/Scripts/SceneControllerSelectWord.cs
+++ Assets/unity1week202403/Scripts/SceneControllerSelectWord.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using R3;
 using UnityEngine;
 
 namespace unity1week202403
@@ -10,9 +11,25 @@ namespace unity1week202403
     /// </summary>
     public sealed class SceneControllerSelectWord : MonoBehaviour
     {
+        [SerializeField]
+        private HKUIDocument selectWordDocumentPrefab;
+
+        private string selectedWord;
+
         private async void Start()
         {
             await BootSystem.IsReady;
+
+            var uiPresenterSelectWord = new UIPresenterSelectWord();
+            uiPresenterSelectWord.Begin(selectWordDocumentPrefab, destroyCancellationToken).Forget();
+            uiPresenterSelectWord.SetWord("");
+            uiPresenterSelectWord.OnSelected
+                .Subscribe(character =>
+                {
+                    selectedWord += character;
+                    uiPresenterSelectWord.SetWord(selectedWord);
+                });
+            uiPresenterSelectWord.BeginInput(destroyCancellationToken);
         }
     }
 }
