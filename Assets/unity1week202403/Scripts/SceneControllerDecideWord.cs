af2ed4333cbfb21980143bc079ccd1fe8b4cf1a2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-23 09:30:17 +0900

    Add new scene and script files for word selection

---
+++ Assets/unity1week202403/Scripts/SceneControllerDecideWord.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using R3;
+using UnityEngine;
+
+namespace unity1week202403
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class SceneControllerDecideWord : MonoBehaviour
+    {
+        [SerializeField]
+        private HKUIDocument selectWordDocumentPrefab;
+
+        private string selectedWord;
+
+        private async void Start()
+        {
+            await BootSystem.IsReady;
+
+            var uiPresenterSelectWord = new UIPresenterSelectWord();
+            uiPresenterSelectWord.BeginAsync(selectWordDocumentPrefab, destroyCancellationToken).Forget();
+            uiPresenterSelectWord.SetWord("");
+            var inputScope = CancellationTokenSource.CreateLinkedTokenSource(destroyCancellationToken);
+            uiPresenterSelectWord.OnSelected
+                .Subscribe(character =>
+                {
+                    selectedWord += character;
+                    uiPresenterSelectWord.SetWord(selectedWord);
+                    if (selectedWord.Length == 4)
+                    {
+                        inputScope.Cancel();
+                        inputScope.Dispose();
+                    }
+                })
+                .RegisterTo(inputScope.Token);
+            uiPresenterSelectWord.BeginInput(inputScope.Token);
+            await UniTask.WaitUntilCanceled(inputScope.Token);
+            Debug.Log(selectedWord);
+        }
+    }
+}
