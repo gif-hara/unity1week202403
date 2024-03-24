4546b112aaa134f9a0f271cdf14c3347b36fc8e4
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-24 09:16:40 +0900

    バトル処理の修正とエラーハンドリングの改善

--- Assets/unity1week202403/Scripts/SceneControllerBattle.cs
+++ Assets/unity1week202403/Scripts/SceneControllerBattle.cs
@@ -27,69 +27,104 @@ namespace unity1week202403
 
         private async void Start()
         {
-            await BootSystem.IsReady;
-            var playerStatus = TinyServiceLocator.TryResolve<ActorStatus>("PlayerStatus");
-            if (playerStatus == null)
+            try
             {
-                playerStatus = debugData.PlayerStatus;
-            }
-            var player = actor.Spawn(playerStatus);
-            var enemyStatus = GetRandomActorStatus();
-            var enemy = actor.Spawn(enemyStatus);
-            var actorQueue = new Queue<Actor>();
-
-            UIPresenterActorName.BeginAsync(actorNameDocumentPrefab, player, enemy, destroyCancellationToken).Forget();
-            UIPresenterStatus.BeginAsync(statusDocumentPrefab, player, enemy, destroyCancellationToken).Forget();
-            var uiPresenterMainMessage = new UIPresenterMainMessage();
-            uiPresenterMainMessage.BeginAsync(mainMessageDocumentPrefab, destroyCancellationToken).Forget();
-
-            await uiPresenterMainMessage.PlayAnimationAsync("Battle Start!", destroyCancellationToken);
-
-            if (player.StatusController.Speed == enemy.StatusController.Speed)
-            {
-                var random = UnityEngine.Random.Range(0, 2);
-                actorQueue.Enqueue(random == 0 ? player : enemy);
-                actorQueue.Enqueue(random == 0 ? enemy : player);
-            }
-            else
-            {
-                actorQueue.Enqueue(player.StatusController.Speed > enemy.StatusController.Speed ? player : enemy);
-                actorQueue.Enqueue(player.StatusController.Speed < enemy.StatusController.Speed ? player : enemy);
-            }
-
-            while (!player.StatusController.IsDead && !enemy.StatusController.IsDead)
-            {
-                var currentActor = actorQueue.Dequeue();
-                var target = currentActor == player ? enemy : player;
-
-                try
+                await BootSystem.IsReady;
+                var playerStatus = TinyServiceLocator.TryResolve<ActorStatus>("PlayerStatus");
+                if (playerStatus == null)
                 {
-                    await currentActor.PerformActionAsync(target, destroyCancellationToken);
-                    await UniTask.Delay(TimeSpan.FromSeconds(1));
+                    playerStatus = debugData.PlayerStatus;
                 }
-                catch (OperationCanceledException)
+                var player = actor.Spawn(playerStatus);
+                var enemyStatus = GetRandomActorStatus();
+                var enemy = actor.Spawn(enemyStatus);
+
+                var uiPresenterActorName = new UIPresenterActorName();
+                uiPresenterActorName.BeginAsync(actorNameDocumentPrefab, player, enemy, destroyCancellationToken).Forget();
+                UIPresenterStatus.BeginAsync(statusDocumentPrefab, player, enemy, destroyCancellationToken).Forget();
+                var uiPresenterMainMessage = new UIPresenterMainMessage();
+                uiPresenterMainMessage.BeginAsync(mainMessageDocumentPrefab, destroyCancellationToken).Forget();
+
+                await uiPresenterMainMessage.PlayAnimationAsync("Battle Start!", destroyCancellationToken);
+
+                // バトル処理
+                while (true)
                 {
-                    break;
-                }
-                catch (Exception e)
-                {
-                    Debug.LogException(e);
-                    break;
+                    await ProccessTurnAsync();
+                    if (player.StatusController.IsDead)
+                    {
+                        await uiPresenterMainMessage.PlayAnimationAsync("You Lose...", destroyCancellationToken);
+                        break;
+                    }
+                    else
+                    {
+                        await uiPresenterMainMessage.PlayAnimationAsync("You Win!", destroyCancellationToken);
+                        player.StatusController.ResetAll();
+                        enemyStatus = GetRandomActorStatus();
+                        enemy.StatusController.Reset(enemyStatus);
+                        uiPresenterActorName.SetEnemyName(enemy.StatusController.Name);
+                    }
                 }
 
-                if (target.StatusController.IsDead)
+                // 終了処理
+
+                static ActorStatus GetRandomActorStatus()
                 {
-                    break;
+                    var wordSpecs = TinyServiceLocator.Resolve<MasterData>().WordSpecs;
+                    var wordSpec = wordSpecs.List[UnityEngine.Random.Range(0, wordSpecs.List.Count)];
+                    return WordCalculator.ToActorStatus(wordSpec.Word);
                 }
 
-                actorQueue.Enqueue(currentActor);
-            }
+                async UniTask ProccessTurnAsync()
+                {
+                    var actorQueue = new Queue<Actor>();
+                    if (player.StatusController.Speed == enemy.StatusController.Speed)
+                    {
+                        var random = UnityEngine.Random.Range(0, 2);
+                        actorQueue.Enqueue(random == 0 ? player : enemy);
+                        actorQueue.Enqueue(random == 0 ? enemy : player);
+                    }
+                    else
+                    {
+                        actorQueue.Enqueue(player.StatusController.Speed > enemy.StatusController.Speed ? player : enemy);
+                        actorQueue.Enqueue(player.StatusController.Speed < enemy.StatusController.Speed ? player : enemy);
+                    }
 
-            static ActorStatus GetRandomActorStatus()
+                    while (!player.StatusController.IsDead && !enemy.StatusController.IsDead)
+                    {
+                        var currentActor = actorQueue.Dequeue();
+                        var target = currentActor == player ? enemy : player;
+
+                        try
+                        {
+                            await currentActor.PerformActionAsync(target, destroyCancellationToken);
+                            await UniTask.Delay(TimeSpan.FromSeconds(1));
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogException(e);
+                            break;
+                        }
+
+                        if (target.StatusController.IsDead)
+                        {
+                            break;
+                        }
+
+                        actorQueue.Enqueue(currentActor);
+                    }
+                }
+            }
+            catch (OperationCanceledException)
             {
-                var wordSpecs = TinyServiceLocator.Resolve<MasterData>().WordSpecs;
-                var wordSpec = wordSpecs.List[UnityEngine.Random.Range(0, wordSpecs.List.Count)];
-                return WordCalculator.ToActorStatus(wordSpec.Word);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
             }
         }
     }
