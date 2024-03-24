98d5b996780ef17e6cf6f5622621e3b12e76f828
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-24 10:53:04 +0900

    unity1week202403/Scripts/SceneControllerResult.csを追加

---
+++ Assets/unity1week202403/Scripts/SceneControllerResult.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace unity1week202403
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class SceneControllerResult : MonoBehaviour
+    {
+        [SerializeField]
+        private BattleStartData debugData;
+
+        private async void Start()
+        {
+            try
+            {
+                await BootSystem.IsReady;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
+}
